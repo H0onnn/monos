03a01318ed089cc711a742f039a2008947aa15a4
H0onnn <dukei2017@naver.com>
2025-10-16 23:01:31 +0900

    feat(tailwind): add tailwindcss global set

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -11,7 +11,7 @@ interface ButtonProps {
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
-      className={className}
+      className={`${className}, h-12 bg-amber-600 cursor-pointer`}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
